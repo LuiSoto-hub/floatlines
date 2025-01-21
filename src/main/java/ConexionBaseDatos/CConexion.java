3a3b204c7db76042c841c3d333f864f99596a58f
sotoc <sotoc@LuiSoto3>
2025-01-20 20:49:46 -0600

    se creo conexion con la base de datos floatLines de mysql agregando dependencies desde el pom.xml

--- src/main/java/ConexionBaseDatos/CConexion.java
+++ src/main/java/ConexionBaseDatos/CConexion.java
@@ -4,10 +4,35 @@
  */
 package ConexionBaseDatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author sotoc
  */
 public class CConexion {
+    Connection conectar =null;
+    String usuario="root";
+    String contrasenia="Joseluischavez5";
+    String bd="FloatLines";
+    String ip="localhost";
+    String puerto="3307";
     
+    String cadena="jbdc:mysql://"+ip+":"+puerto+"/"+bd;
+    
+    public Connection establecerConexion(){
+    try{
+        Class.forName("com.mysql.jdb.Driver");
+        conectar=DriverManager.getConnection(cadena,usuario,contrasenia);
+        JOptionPane.showMessageDialog(null, "se conecto a la base de datos");
+        
+    }catch(Exception e){
+        JOptionPane.showMessageDialog(null, "error al conectar base de datos:"+e.toString());
+    }
+   return conectar;
+    
+    }
 }
+
